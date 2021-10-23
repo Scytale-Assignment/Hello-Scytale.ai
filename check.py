0579453be0734f49e25b64f7877ced3e9f19d3b6
Talraviv <93025096+Talraviv@users.noreply.github.com>
2021-10-23 21:24:28 +0300

    Update check.py

--- check.py
+++ check.py
@@ -2,3 +2,19 @@ print("check branch")
 
 print("1+1")
 print("check branch 2")
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def subtract(a, b):
+    return a + b  # <--- fix this in step 7
+
+
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
